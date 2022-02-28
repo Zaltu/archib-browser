5294770240c8fb7055422f5dd4b73e46eefdfcd1
Zaltu <swwouf@hotmail.com>
2022-02-28 14:10:38 -0500

    search cat template setup

---
+++ src/script/filtertemplates.js
@@ -0,0 +1,93 @@
+AUDIOFILTER = `
+`
+
+BOOKFILTER = `
+`
+
+GAMEFILTER = `
+<div class="row">
+    <div class="six columns">
+        <label for="platform">Platform</label>
+        <input type="text" id="platform" name="platform">
+    </div>
+    <div class="six columns">
+        <label for="drm">DRM</label>
+        <input type="text" id="drm" name="drm">
+    </div>
+</div>
+<div class="row">
+    <div class="six columns">
+        <label for="developer">Developer</label>
+        <input type="text" id="developer" name="developer">
+    </div>
+    <div class="six columns">
+        <label for="publisher">Publisher</label>
+        <input type="text" id="publisher" name="publisher">
+    </div>
+</div>
+<div class="row">
+    <div class="six columns">
+        <label for="year">Release Year</label>
+        <input placeholder="- - - -" type="number" id="year" name="year">
+    </div>
+    <div class="six columns">
+        <label for="platformid">ID</label>
+        <input type="text" id="platformid" name="platformid">
+    </div>
+</div>
+<div class="row">
+    <label for="genre">Genre(s)</label>
+    <input type="text" id="genre" name="genre">
+</div>
+<div class="typeselect">
+    <label for="adult">Adult Only</label>
+    <input type="checkbox" id="adult" name="adult">
+</div>
+`
+
+IMAGESETFILTER = `
+`
+
+SOFTWAREFILTER = `
+`
+
+VIDEOFILTER = `
+<div class="row">
+    <div class="six columns">
+        <label for="style">Style</label>
+        <input type="text" id="style" name="style">
+    </div>
+    <div class="six columns">
+        <label for="drm">DRM</label>
+        <input type="text" id="drm" name="drm">
+    </div>
+</div>
+<div class="row">
+    <div class="six columns">
+        <label for="dev">Developer</label>
+        <input type="text" id="developer" name="dev">
+    </div>
+    <div class="six columns">
+        <label for="publisher">Publisher</label>
+        <input type="text" id="publisher" name="publisher">
+    </div>
+</div>
+<div class="row">
+    <div class="six columns">
+        <label for="year">Release Year</label>
+        <input placeholder="- - - -" type="number" id="year" name="year">
+    </div>
+    <div class="six columns">
+        <label for="platformid">ID</label>
+        <input type="text" id="platformid" name="platformid">
+    </div>
+</div>
+<div class="row">
+    <label for="genre">Genre(s)</label>
+    <input type="text" id="genre" name="genre">
+</div>
+<div class="typeselect">
+    <label for="adult">Adult Only</label>
+    <input type="checkbox" id="adult" name="adult">
+</div>
+`
